04aaadf347cacd5aacbfc23f1c124ed5af64c2e0
bezalabernaya <85689689+bezalabernaya@users.noreply.github.com>
2022-12-19 19:46:56 +0300

    Add files via upload

---
+++ vectorstr/vectorstr.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "Russian");
+    string x;//вводимые буквы
+    cout << " Введите буквы: " << endl;
+    vector <string> str;
+
+    for (; ;) {
+        cin >> x;
+        string h = x;
+        if (h == "end") {
+            break;
+        }
+        else {
+            str.insert(str.end(), x); 
+        }
+    }
+
+    sort(str.begin(), str.end());
+
+  
+        for (size_t m = 0; m < str.size(); m++) {
+            cout << str[m] << " ";
+        }
+   
+    
+
+	return 0;
+}
